e97a8765dd8145cb8501a99f3e2147a2d8c74644
Scott <smclendening@gmail.com>
2017-11-18 15:26:40 -0800

    Add initial tests for games endpoint

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/users', (req, res) => {
+  res.status(200).json([]);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json({username: 'scott'})
+})
+
 module.exports = app;
--- test/server/index.js
+++ test/server/index.js
@@ -3,12 +3,12 @@ const expect = require('chai').expect();
 const request = require('supertest');
 const app = require('../../app.js');
 
+let user = {
+  username: 'scott'
+}
 
 describe('/users', () => {
-  
-  let user = {
-    username: 'scott'
-  }
+
 
   describe('POST /users', () => {
     it('should create a user', () => {
@@ -38,7 +38,7 @@ describe('/users', () => {
   });
 
   describe('GET /users/:id', () => {
-    it('should get a specific users', () => {
+    it('should get a specific user', () => {
       request(app)
         .get(`users/${user.id}`)
         .end((err, res) => {
@@ -65,8 +65,101 @@ describe('/users', () => {
 
 describe('/games', () => {
 
-  describe('')
-})
+  let game;
+
+  let userTwo = {
+    username: 'mike',
+    id: 2 // TODO: replace this with user in db
+  }
+
+  const moveOne = {
+    user: user.id,
+    row: 0,
+    col: 1
+  }
+
+  describe('POST /games', () => {
+    it('should create a game', () => {
+      request(app)
+        .post('/games')
+        .send(user.id)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(201);
+          expect(res.body.x_player_id).to.equal(user.id);
+          game = res.body;
+          done();
+        });
+    })
+  });
+
+  describe('POST /games/:id/users', () => {
+    it('should add a user to a game', () => {
+      request(app)
+        .post(`/games/${game.id}/users`)
+        .send(userTwo.id)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(201);
+          expect(res.body.o_player_id).to.equal(userTwo.id);
+          expect(res.body.status).to.equal('active'); // game should now be active
+          game = res.body;
+          done();
+        });
+    });
+  });
+
+  describe('GET /games', () => {
+    it('should get all games', () => {
+      request(app)
+        .get('/games')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an('array');
+          expect(res.body[0].id).to.equal(game.id);
+          done();
+        });
+    });
+  });
+
+  describe('GET /games/:id', () => {
+    it('should get a specific game', () => {
+      request(app)
+        .get(`/games/${game.id}`)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body.id).to.equal(game.id);
+          done();
+        });
+    });
+  });
+
+  describe('POST /games/:id/moves', () => {
+    it('should make a move in a game', () => {
+      request(app)
+        .post(`/games/${game.id}/moves`)
+        .send(moveOne)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(201);
+          expect(res.body.board).to.be.an('array'); // TODO: check specific location
+          done();
+        });
+    });
+  });
+
+  describe('DELETE /games', () => {
+    it('should delete a game', () => {
+      request(app)
+        .delete(`games/${game.id}`)
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body.message).to.equal('Game successfully deleted.');
+          done();
+        });
+    });
+  });
+
+});
+
+
 
 
 
