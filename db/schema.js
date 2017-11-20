a52615f55b4c46bd923e6efd4b9f16f82a309140
Scott <smclendening@gmail.com>
2017-11-19 18:21:55 -0800

    Only create enumerated types if they do not exist

--- db/schema.js
+++ db/schema.js
@@ -1,32 +1,43 @@
 module.exports = db => (
+
   db.query('CREATE TABLE IF NOT EXISTS users(\
     user_id SERIAL PRIMARY KEY,\
     username VARCHAR(30) NOT NULL UNIQUE,\
     created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP\
     );')
   .then(() => (
-    db.query('CREATE TYPE game_status AS ENUM(\
-    waiting, active, complete\
-    );')
+    db.query('SELECT 1 from pg_type where typname = $1', ['game_status'])
+    .then((results) => {
+      if (!results.length) {
+        return db.query('CREATE TYPE game_status AS ENUM(\
+        $1, $2, $3)\
+        ', ['waiting', 'active', 'complete'])
+      }
+    })
   ))
   .then(() => (
-    db.query('CREATE TYPE player_type AS ENUM(\
-    X, O, none\
-    );')
+    db.query('SELECT 1 from pg_type where typname = $1', ['player_type'])
+    .then((results) => {
+      if (!results.length) {
+        return db.query('CREATE TYPE player_type AS ENUM(\
+        $1, $2, $3)\
+        ', ['X', 'O', 'none'])
+      }
+    })
   ))
   .then(() => (
     db.query('CREATE TABLE IF NOT EXISTS games(\
     game_id SERIAL PRIMARY KEY,\
-    status game_status DEFAULT waiting,\
-    winner player_type DEFAULT none,\
+    status game_status DEFAULT $1,\
+    winner INT REFERENCES users ON DELETE CASCADE,\
     board JSONB,\
     x_player_id INT REFERENCES users ON DELETE CASCADE,\
     o_player_id INT REFERENCES users ON DELETE CASCADE,\
     created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,\
     last_player_move player_type\
-    );')
+    )', ['waiting'])
   ))
-  .then(() => (
-    console.log('schema has been run! wow')
-  ))
+  .catch(err => {
+    console.log('ERROR IN SCHEMA: ', err)
+  })
 )
