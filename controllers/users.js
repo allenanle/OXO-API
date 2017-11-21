4fb3a167b4bff52f7e555853722b97bf0469e291
Scott <smclendening@gmail.com>
2017-11-20 16:51:48 -0800

    Set up routes for User controller

---
+++ controllers/users.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users.js');
+
+router.post('/', (req, res) => {
+  const { username } = req.body;
+
+  if (!username) {
+    return res.status(400).send('A username is required to create a user.');
+  }
+
+  return User.new(username)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(400).send('A player with that username already exists.');
+    })
+})
+
+router.get('/', (req, res) => {
+  return User.getAll()
+    .then(users => {
+      res.status(200).send(users);
+    })
+    .catch(err => {
+      res.status(400).send(err.message);
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  // TODO: what if id does not exist - check err message
+  return User.getById(id)
+    .then(user => {
+      res.status(200).send(user);
+    })
+    .catch(err => {
+      res.status(400).send(err.message);
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  return User.delete(id)
+    .then(user => {
+      res.status(200).send('User successfully deleted.');
+    })
+    .catch(err => {
+      res.status(400).send(err.message);
+    })
+})
+
+module.exports = router;
